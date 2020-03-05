98328bce6000a214ba2a2a83404bd35bcc1f0185
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-05 11:11:15 -0600

    Exporting.. but now with inheritance :)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 import {name,credit,showInfo,Student} from './cliente.js'
-import {company,credit as creditCompany,bussiness,showInfo as infoCompany} from './empresa.js'
+import {company,credit as creditCompany,bussiness,showInfo as infoCompany,Fullcompany} from './empresa.js'
 document.getElementById('app').innerHTML=showInfo()
 
 let student1=new Student('Raul','Guillen','UNIExample')
@@ -7,3 +7,7 @@ let student1=new Student('Raul','Guillen','UNIExample')
 document.getElementById('app').innerHTML+=`<br> ${student1.showData()}`
 
 document.getElementById('app').innerHTML+=`<br> ${infoCompany()}`
+
+let newCompany=new Fullcompany('MOODLE','Comunity of users',4000000,'development for education')
+
+document.getElementById('app').innerHTML+=`<br> ${newCompany.showData()}`
--- js/empresa.js
+++ js/empresa.js
@@ -1,7 +1,21 @@
+import {Student} from './cliente.js'
 export let company='moodle'
+export let companyOwner='Comunity'
 export let credit=500000
 export let bussiness='E-Learning'
 //Exporting a function
 export function showInfo(){
     return `Yesterday, the company ${company} has ${credit} of available credit to projects of ${bussiness}`
+}
+
+export class Fullcompany extends Student{
+    constructor(company,companyOwner,credit,bussiness){
+        super(credit)
+        this.company=company
+        this.companyOwner=companyOwner
+        this.bussiness=bussiness
+    }
+    showData(){
+        return `The owner of the company ${this.company} is  ${this.companyOwner} and they are dedicated to ${this.bussiness}`
+    }
 }
