a2979ff2687b4274699267ada1e28a1ff1b4d978
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-05 10:14:06 -0600

    Now exporting from 2 files to main js file

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 import {name,credit,showInfo,Student} from './cliente.js'
+import {company,credit as creditCompany,bussiness,showInfo as infoCompany} from './empresa.js'
 document.getElementById('app').innerHTML=showInfo()
 
 let student1=new Student('Raul','Guillen','UNIExample')
 
 document.getElementById('app').innerHTML+=`<br> ${student1.showData()}`
+
+document.getElementById('app').innerHTML+=`<br> ${infoCompany()}`
---
+++ js/empresa.js
@@ -0,0 +1,7 @@
+export let company='moodle'
+export let credit=500000
+export let bussiness='E-Learning'
+//Exporting a function
+export function showInfo(){
+    return `Yesterday, the company ${company} has ${credit} of available credit to projects of ${bussiness}`
+}
