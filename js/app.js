c2b24d9fd13a077846a839841004fc5d031e552e
ing-arriola <ingeniero.arriola@gmail.com>
2020-03-05 10:00:03 -0600

    Added class exported

--- js/app.js
+++ js/app.js
@@ -1,2 +1,6 @@
-import {name,credit,showInfo} from './cliente.js'
+import {name,credit,showInfo,Student} from './cliente.js'
 document.getElementById('app').innerHTML=showInfo()
+
+let student1=new Student('Raul','Guillen','UNIExample')
+
+document.getElementById('app').innerHTML+=`<br> ${student1.showData()}`
--- js/cliente.js
+++ js/cliente.js
@@ -1,6 +1,17 @@
 export let name='jaime'
 export let credit=500
-
+//Exporting a function
 export function showInfo(){
     return `Yesterday, the user ${name} has ${credit} of available credit`
+}
+//Exporting a class
+export class Student{
+    constructor(name,surname,institution){
+        this.name=name
+        this.surname=surname
+        this.institution=institution
+    }
+    showData(){
+        return `${this.name} ${this.surname} studies at ${this.institution}`
+    }
 }
